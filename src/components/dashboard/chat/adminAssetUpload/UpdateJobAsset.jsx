e57a3ce1627c0fdfb9a6390f2b0776e52e6ae3d7
Wasif Munir <wasif.munir@droppgroup.xyz>
2024-02-17 19:05:06 +0500

    update asset

--- src/components/dashboard/chat/adminAssetUpload/UpdateJobAsset.jsx
+++ src/components/dashboard/chat/adminAssetUpload/UpdateJobAsset.jsx
@@ -203,48 +203,52 @@ export default function UpdateJobAsset({
                   </div>
                 )}
               </div>
-              <div className='flex justify-start gap-2 h-5 mt-2'>
+              {uploadedVideoCount != 0 ? (
+                <div className='flex justify-start gap-2 h-5 mt-2'>
               
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  onClick={() => {
-                    if (savedVideos !== null) {
-                      handleVideoDownload();
-                    } else if (uploadedVideoCount > 0) {
-                      handleVideoDownload();
-                    } else {
-                      handleGlbDownload();
-                    }
-                  }}
-                  className='cursor-pointer'
-                >
-                  <path
-                    d='M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15'
-                    stroke='#FFFFFF'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  />
-                  <path
-                    d='M7 10L12 15L17 10'
-                    stroke='#FFFFFF'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  />
-                  <path
-                    d='M12 15V3'
-                    stroke='#FFFFFF'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                  />
-                </svg>
-              </div>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    onClick={() => {
+                      if (savedVideos !== null) {
+                        handleVideoDownload();
+                      } else if (uploadedVideoCount > 0) {
+                        handleVideoDownload();
+                      } else {
+                        handleGlbDownload();
+                      }
+                    }}
+                    className='cursor-pointer'
+                  >
+                    <path
+                      d='M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15'
+                      stroke='#FFFFFF'
+                      strokeWidth='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                    <path
+                      d='M7 10L12 15L17 10'
+                      stroke='#FFFFFF'
+                      strokeWidth='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                    <path
+                      d='M12 15V3'
+                      stroke='#FFFFFF'
+                      strokeWidth='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                  </svg>
+
+                </div>
+              ) : null}
+              
             </div>
           </div>
         </div>
