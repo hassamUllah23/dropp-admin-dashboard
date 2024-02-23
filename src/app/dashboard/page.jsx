425d4e34d78010c2bb522fdb46a539f74c9e36b6
= <ahsan@droppgroup.xyz>
2024-02-23 18:06:58 +0500

    notification update

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,53 +1,132 @@
-'use client';
-import React, { useState, useEffect, useCallback} from 'react';
-import SingleJob from '@/components/dashboard/SingleJob';
-import useApiHook from '@/hooks/useApiHook';
+"use client";
+import React, { useState, useEffect, useMemo } from "react";
+import SingleJob from "@/components/dashboard/SingleJob";
+import useApiHook from "@/hooks/useApiHook";
+import { RotatingLines } from 'react-loader-spinner';
+
 export default function Dashboard() {
   const { handleApiCall, isApiLoading } = useApiHook();
   const [allJobs, setAllJobs] = useState([]);
-  let url ="/jobs/all/admin";
-  
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  let url = `/jobs/all/admin?page=${page}&pageSize=${pageSize}`;
+
   const loadJobs = async () => {
     const result = await handleApiCall({
-      method: 'GET',
+      method: "GET",
       url: url,
     });
-    
     setAllJobs(result?.data?.jobs);
+    console.log('result', result)
+    await calculatePageCount(result?.data?.count);
   };
-  
+
+  const calculatePageCount = async (count) => { 
+    console.log('count',count)
+    setCount(count);
+    setPageCount(Math.ceil(count / pageSize));
+  };
+  const handlePageSizeChange = (newPage) => {
+    setPage(newPage);
+  };
+  const pageOptions = useMemo(() => {
+    const roundedTotal = Math.ceil(count / pageSize);
+
+    return Array.from({ length: roundedTotal }, (_, index) => (index + 1) * 10);
+  }, [count, pageSize]);
+
   useEffect(() => {
-    loadJobs()
+    loadJobs();
   }, []);
 
+  useEffect(() => {
+    loadJobs();
+  }, [page, pageSize]);
+  // console.log(page, count, pageCount, pageSize)
   return (
-    <div className='p-2.5 pt-4 md:pt-10 max-w-screen-3xl h-fu w-full m-auto flex flex-col min-w-80 z-10 text-white'>
-      <div className='flex justify-end mb-2 md:mb-5 pr-3 md:pr-10'>
-        <button className=' text-sm px-5 lightGrayBg leading-10 rounded-2xl flexCenter'>
+    <div className="p-2.5 pt-4 md:pt-10 max-w-screen-3xl h-fu w-full m-auto flex flex-col min-w-80 z-10 text-white">
+      <div className="flex justify-end mb-2 md:mb-5 pr-3 md:pr-10">
+        <button className=" text-sm px-5 lightGrayBg leading-10 rounded-2xl flexCenter">
           <svg
-            width='24'
-            height='24'
-            className='w-6 h-6 mr-2'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth='1.5'
-            stroke='currentColor'
+            width="24"
+            height="24"
+            className="w-6 h-6 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
           >
             <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z'
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
             />
           </svg>
           <span>Filters</span>
         </button>
       </div>
-
-      <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  px-3 md:px-10 py-3 gap-4 md:gap-6'>
-        {[...allJobs]?.reverse()?.map((job) => (
-          <SingleJob key={job._id} jobKeys={job}/>
+      <div className="grid grid-cols-3 gap-x-4 items-center px-5 mx-5 mb-2">
+        <div>
+          <span className="text-base leading-5">
+            Showing {page} - {pageCount} of {count}
+          </span>
+        </div>
+        <div className="flex justify-center">
+          {isApiLoading && (
+            <RotatingLines
+              height="28"
+              width="28"
+              color="blue"
+              strokeWidth="5"
+              animationDuration="0.75"
+              ariaLabel="rotating-lines-loading"
+            />
+          )}
+        </div>
+        <div className="flex justify-end">
+          {page > 1 && (
+            <button
+              className="text-sm px-3 lightGrayBg leading-8 rounded-xl flexCenter hover:bg-white hover:text-black"
+              onClick={() => handlePageSizeChange(page - 1)}
+              disabled={page === 1}
+            >
+              Previous
+            </button>
+          )}
+          {page < pageCount && (
+            <button
+              className="text-sm px-3 lightGrayBg leading-8 rounded-xl flexCenter ml-2 hover:bg-white hover:text-black"
+              onClick={() => handlePageSizeChange(page + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  px-3 md:px-10 py-3 gap-4 md:gap-6">
+        {allJobs?.map((job) => (
+          <SingleJob key={job._id} jobKeys={job} />
         ))}
+      </div>
+      <div className="flex justify-end items-center px-5 mx-5">
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPage(1);
+            setPageSize(Number(e.target.value));
+          }}
+          className="text-sm p-2 lightGrayBg leading-10 rounded-2xl flexCenter mr-2 justify-center"
+        >
+          {pageOptions.map((size, index) => (
+            <option key={index} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+        per page
       </div>
     </div>
   );
--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,7 @@
-export const NEXT_PUBLIC_API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'https://018a-2400-adc5-459-6b00-a4f1-9157-edd0-5e11.ngrok-free.app/api/v1';
+// export const NEXT_PUBLIC_API_URL =
+  // process.env.NEXT_PUBLIC_API_URL || 'https://solutionsloftmail.com/api/v1';
+
+  export const NEXT_PUBLIC_API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'https://cb58-139-135-38-175.ngrok-free.app/api/v1';
 export const NEXT_PUBLIC_CHAT_API_URL =
   process.env.NEXT_PUBLIC_CHAT_API_URL || 'https://solutionsloftllc.com/api';
