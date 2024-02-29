42fb2266e1d54ed106bd2741eab21fa8ea172645
M Irfan Ali <irfan.ali@droppgroup.xyz>
2024-02-29 17:31:33 +0500

    Fixes the config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const NEXT_PUBLIC_API_URL = 'http://localhost:4800/api/v1';
-// process.env.NEXT_PUBLIC_API_URL || 'https://solutionsloftmail.com/api/v1';
+export const NEXT_PUBLIC_API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'https://solutionsloftmail.com/api/v1';
 export const NEXT_PUBLIC_CHAT_API_URL =
   process.env.NEXT_PUBLIC_CHAT_API_URL || 'https://solutionsloftllc.com/api';
