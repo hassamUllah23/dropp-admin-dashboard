873116150a49ae7e1905ab7fe8c6a3609c59c57a
= <ahsan@droppgroup.xyz>
2024-03-06 16:20:55 +0500

    update

--- src/app/active-users/page.jsx
+++ src/app/active-users/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import useApiHook from "@/hooks/useApiHook";
+import { RotatingLines } from 'react-loader-spinner';
+
 const page = () => {
   const { handleApiCall, isApiLoading } = useApiHook();
   const [searchValue, setSearchValue] = useState("");
@@ -8,7 +10,7 @@ const page = () => {
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(0);
   const [pageCount, setPageCount] = useState(0);
-  const [pageSize] = useState(12);
+  const [pageSize] = useState(10);
   const [employees, setEmpoyees] = useState([]);
   let url = `/employee/all-users?page=${page}&pageSize=${pageSize}`;
 
@@ -61,12 +63,24 @@ const page = () => {
 
   return (
     <div className="px-3 md:px-14 py-6 max-w-screen-3xl h-full w-full m-auto flex flex-col min-w-80 z-10 text-white">
-      <div className="flex justify-between items-center">
-        <h1 className="text-[20px] font-[700] leading-[23.48px]">
+      <div className="grid grid-cols-3 m-4">
+        <h1 className="flex items-center text-[20px] font-[700] leading-[23.48px]">
           Active users
         </h1>
+        <div className="flex justify-center items-center">
+          {isApiLoading && (
+            <RotatingLines
+            height="24"
+            width="24"
+            color="blue"
+            strokeWidth="5"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+          />
+          )}
+        </div>
         {filteredEmployees.length > 0 && (
-          <div className="max-w-[334px] w-full flex items-center border border-white rounded-lg p-3">
+          <div className="max-w-[334px] w-full flex items-center border border-white rounded-lg p-2">
             <input
               type="text"
               id="search"
@@ -86,92 +100,68 @@ const page = () => {
           </div>
         )}
       </div>
-      {isApiLoading ? (
-        <div className="w-full flex items-center justify-center">
-          <svg
-            className="animate-spin -ml-1 mr-3 h-16 w-16 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </div>
-      ) : (
-        <table className="text-white w-full border border-transparent mt-4 mb-6">
-          <thead className="sticky top-0 bg-[#262626] text-white rounded-[4px]">
-            <tr className="">
-              <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] text-left rounded-l-[4px]">
-                Employee Name
-              </th>
-              <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] text-left">
-                Employee Email
-              </th>
-              <th className="py-2 px-4 text-[10px] text-[#FFFFFF] leading-[11.74px]">
-                Last Activity
-              </th>
-              <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px]">
-                Status
-              </th>
-              <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] rounded-r-[4px]">
-                Actions
-              </th>
+      
+      <table className="text-white w-full border border-transparent mb-6">
+        <thead className="sticky top-0 bg-[#262626] text-white rounded-[4px]">
+          <tr className="">
+            <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] text-left rounded-l-[4px]">
+              Employee Name
+            </th>
+            <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] text-left">
+              Employee Email
+            </th>
+            <th className="py-2 px-4 text-[10px] text-[#FFFFFF] leading-[11.74px]">
+              Last Activity
+            </th>
+            <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px]">
+              Status
+            </th>
+            <th className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] rounded-r-[4px]">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredEmployees.length > 0 ? (
+            filteredEmployees.map((item, index) => {
+              return (
+                <tr key={index} className="my-3 row w-full darkGrayBg">
+                  <td className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] rounded-l-[4px]">
+                    {`${item.firstName} ${item.lastName}`}
+                  </td>
+                  <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px]">
+                    {item.email}
+                  </td>
+                  <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px] text-center">
+                    22-03-2024
+                  </td>
+                  <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px] text-center">
+                    {item.status}
+                  </td>
+                  <td className="py-4 px-2 rounded-r-[4px]">
+                    <div className="flex justify-center items-center">
+                      <button className="cursor-pointer bg-[#850101] p-1 rounded-[4px] ">
+                        <img
+                          src="/trash.svg"
+                          alt="delete_icon"
+                          width={16}
+                          height={16}
+                        />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan="5" className="pl-1">
+                No data found
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredEmployees.length > 0 ? (
-              filteredEmployees.map((item, index) => {
-                return (
-                  <tr key={index} className="my-3 row w-full darkGrayBg">
-                    <td className="py-4 px-2 text-[10px] text-[#FFFFFF] leading-[11.74px] rounded-l-[4px]">
-                      {`${item.firstName} ${item.lastName}`}
-                    </td>
-                    <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px]">
-                      {item.email}
-                    </td>
-                    <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px] text-center">
-                      22-03-2024
-                    </td>
-                    <td className="py-4 px-2 text-[10px] text-[#808080] leading-[11.74px] text-center">
-                      {item.status}
-                    </td>
-                    <td className="py-4 px-2 rounded-r-[4px]">
-                      <div className="flex justify-center items-center">
-                        <button className="cursor-pointer bg-[#850101] p-1 rounded-[4px] ">
-                          <img
-                            src="/trash.svg"
-                            alt="delete_icon"
-                            width={16}
-                            height={16}
-                          />
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <tr>
-                <td colSpan="5" className="pl-1">
-                  No data found
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      )}
+          )}
+        </tbody>
+      </table>
       {pageCount > 1 && (
         <div className="flex gap-[10px] items-center justify-center">
           {page > 1 && (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,81 @@ export const JOB_REJECTED = "rejected";
 export const JOB_GENERATING = "generating";
 export const JOB_COMPLETED = "completed";
 export const JOB_IN_QUEUE = "in-queue";
+
+export const dummyUsers = [
+    {
+        "_id": "65e5a87a8edbef321a9fac3c",
+        "firstName": "John",
+        "lastName": "Doe",
+        "userName": "johndoe123",
+        "email": "johndoe123@gmail.com",
+        "password": "$2b$10$0237zBBbCCT/3acMs150vuzoCat//9i9GBWqO1Yjs8Ic1HXzYN0Bq",
+        "avatar": null,
+        "role": "user",
+        "firebaseTokens": [
+            {
+                "type": "ios",
+                "token": "e_A0fhQ2T0RMhjJACfy9cF:APA91bHrlw_syxePL6VU2-kjk2VKIDOr9lXyl95psCRI-pqkv47eGFHwi8XcXQObukfnr0PYYfjKZn9fWHqBuPQUXnH_m4hfeyCTgvct_JFBOxxofHmTdv11lhmMy-gGK2aMcd5hUUSU"
+            }
+        ],
+        "status": "active",
+        "createdAt": "2024-03-04T10:54:50.849Z",
+        "updatedAt": "2024-03-04T14:07:30.754Z",
+        "id": "65e5a87a8edbef321a9fac3c"
+    },
+    {
+        "_id": "65e5a7148edbef321a9fac31",
+        "firstName": "Jane",
+        "lastName": "Smith",
+        "userName": "janesmith456",
+        "email": "janesmith456@gmail.com",
+        "password": "$2b$10$oVvc/87nzG2pihHbmyAf8u3llJqbSW.YBUyy14FtDOjmhmx8Bw4AC",
+        "avatar": null,
+        "role": "user",
+        "firebaseTokens": [
+            {
+                "type": "ios",
+                "token": "e_A0fhQ2T0RMhjJACfy9cF:APA91bHrlw_syxePL6VU2-kjk2VKIDOr9lXyl95psCRI-pqkv47eGFHwi8XcXQObukfnr0PYYfjKZn9fWHqBuPQUXnH_m4hfeyCTgvct_JFBOxxofHmTdv11lhmMy-gGK2aMcd5hUUSU"
+            }
+        ],
+        "status": "active",
+        "createdAt": "2024-03-04T10:48:52.418Z",
+        "updatedAt": "2024-03-04T10:48:53.964Z",
+        "id": "65e5a7148edbef321a9fac31"
+    },
+    {
+        "_id": "65e581bf6af8c4eb6a31d2b6",
+        "firstName": "Michael",
+        "lastName": "Johnson",
+        "userName": "michaeljohnson789",
+        "email": "michaeljohnson789@gmail.com",
+        "password": "$2b$10$FFdLahxKc7M9vAtC8eoEgOoqre4PF6EicbfHPE5RxnlQY5dJV6OMG",
+        "avatar": null,
+        "role": "user",
+        "firebaseTokens": [
+            {
+                "type": "web",
+                "token": "fdY6Xo0I9Npr_UVNv87GWt:APA91bEGkYvRMdTEIzxy_QJSp34CTV23TBCW_38zNf5CO3dJEWDN78HA2Vc3G4o6_ffQH_8kmHfPr7CqM94mNp7hl4_Bw0D5tOK5WI-CCOqMEXcrbFFAgdsiCNSSRHwm5tLuOFcxtzGF"
+            }
+        ],
+        "status": "active",
+        "createdAt": "2024-03-04T08:09:35.247Z",
+        "updatedAt": "2024-03-04T14:49:34.362Z",
+        "id": "65e581bf6af8c4eb6a31d2b6"
+    },
+    {
+        "_id": "65e57a4d33a494f7f0c84d00",
+        "firstName": "Emily",
+        "lastName": "Williams",
+        "userName": "emilywilliams101",
+        "email": "emilywilliams101@gmail.com",
+        "password": "$2b$10$EmQm5kgr2WvJKCxVNgGAz.Ot2j8sVNLftjE8B0zwOceTawyft72va",
+        "avatar": null,
+        "role": "user",
+        "firebaseTokens": [],
+        "status": "active",
+        "createdAt": "2024-03-04T07:37:49.331Z",
+        "updatedAt": "2024-03-05T08:34:34.507Z",
+        "id": "65e57a4d33a494f7f0c84d00"
+    },
+]
