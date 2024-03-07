62cbbf846a14cddd5106c72e8586121d25c3c89c
M Irfan Ali <irfan.ali@droppgroup.xyz>
2024-03-07 11:26:25 +0500

    Fixes the tokenization of models to mainnet in cms

--- src/utils/tokenisation-helpers.js
+++ src/utils/tokenisation-helpers.js
@@ -2,32 +2,70 @@ import Web3 from 'web3';
 import { ControlNetABI, ControlNetAddress } from './tokenisation-constants';
 import { toast } from 'react-toastify';
 
+/*********** Tokenization Of Model **************/
 export const tokenization = async (url) => {
   try {
-    const provider = new Web3(window.ethereum);
+    const web3 = new Web3(window.ethereum);
     await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const accounts = await provider.eth.getAccounts();
-    const userAddress = accounts[0];
-    const nonce = await provider.eth.getTransactionCount(userAddress, 'latest');
-    const contract = new provider.eth.Contract(
-      ControlNetABI,
-      ControlNetAddress
-    );
+    const contract = new web3.eth.Contract(ControlNetABI, ControlNetAddress);
+    /********** Fetch Accounts **************/
+    const accounts = await web3.eth.getAccounts();
+    const adminAccount = accounts[0];
+    let currentGasPrice = await web3.eth.getGasPrice();
+    let adjustedGasPrice =
+      (BigInt(currentGasPrice) * BigInt(110)) / BigInt(100);
+    adjustedGasPrice = adjustedGasPrice.toString();
+    const estimatedGas = await contract.methods
+      .modelTokenization(adminAccount, url)
+      .estimateGas({ from: adminAccount });
     const txOptions = {
-      from: userAddress,
-      nonce: nonce,
+      from: adminAccount,
+      to: ControlNetAddress,
+      data: contract.methods.modelTokenization(adminAccount, url).encodeABI(),
+      gas: estimatedGas.toString(),
+      gasPrice: adjustedGasPrice,
     };
-
-    const tx = await contract.methods
-      .modelTokenization(userAddress, url)
-      .send(txOptions);
-    const receipt = await provider.eth.getTransactionReceipt(
-      tx.transactionHash
-    );
-    toast.success(`Transaction submitted! Hash: ${tx.transactionHash}`);
+    const txReceipt = await web3.eth.sendTransaction(txOptions);
+    toast.success(`Transaction submitted! Hash: ${txReceipt.transactionHash}`);
 
     return {
-      url: `https://polygonscan.com/tx/${receipt.transactionHash}`,
+      url: `https://polygonscan.com/tx/${txReceipt.transactionHash}`,
+    };
+  } catch (error) {
+    toast.error('Error submitting Tokenization transaction:', error);
+    throw error;
+  }
+};
+
+/************* Transfer Of Model ***************/
+export const transferModel = async (userAddress, tokenId) => {
+  try {
+    const web3 = new Web3(window.ethereum);
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const contract = new web3.eth.Contract(ControlNetABI, ControlNetAddress);
+    /********** Fetch Accounts **************/
+    const accounts = await web3.eth.getAccounts();
+    const adminAccount = accounts[0];
+    let currentGasPrice = await web3.eth.getGasPrice();
+    let adjustedGasPrice =
+      (BigInt(currentGasPrice) * BigInt(110)) / BigInt(100);
+    adjustedGasPrice = adjustedGasPrice.toString();
+    const estimatedGas = await contract.methods
+      .transferFrom(adminAccount, userAddress, tokenId)
+      .estimateGas({ from: adminAccount });
+    const txOptions = {
+      from: adminAccount,
+      to: ControlNetAddress,
+      data: contract.methods
+        .transferFrom(adminAccount, userAddress, tokenId)
+        .encodeABI(),
+      gas: estimatedGas.toString(),
+      gasPrice: adjustedGasPrice,
+    };
+    const txReceipt = await web3.eth.sendTransaction(txOptions);
+    toast.success(`Transaction submitted! Hash: ${txReceipt.transactionHash}`);
+    return {
+      url: `https://polygonscan.com/tx/${txReceipt.transactionHash}`,
     };
   } catch (error) {
     toast.error('Error submitting Tokenization transaction:', error);
