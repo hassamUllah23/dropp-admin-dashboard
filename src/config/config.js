b65f775242e578f2fcc34c9a937dffa21673d2a5
ahsangit config --global user.name "ahsan <ahsan@droppgroup.xyz>
2024-04-19 16:55:20 +0500

    change job status

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-// export const NEXT_PUBLIC_API_URL =
-//   process.env.NEXT_PUBLIC_API_URL || "https://slgo.online/api/v1";
 export const NEXT_PUBLIC_API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://e997-2400-adc5-459-6b00-c0e0-7d39-5c01-5e06.ngrok-free.app/api/v1";
+  process.env.NEXT_PUBLIC_API_URL || "https://slgo.online/api/v1";
+// export const NEXT_PUBLIC_API_URL =
+//   process.env.NEXT_PUBLIC_API_URL || "https://17ef-2400-adc5-459-6b00-6d56-5b60-6010-c676.ngrok-free.app/api/v1";
 export const NEXT_PUBLIC_CHAT_API_URL =
   process.env.NEXT_PUBLIC_CHAT_API_URL || "https://droppthink.com/api";
