b92e89c51568b59913ef2dc512ef0b1fe0a05655
Wasif Munir <wasif.munir@droppgroup.xyz>
2024-05-20 16:42:31 +0500

    fix double audio icon on dashboard on cms

--- src/components/dashboard/SingleJob.jsx
+++ src/components/dashboard/SingleJob.jsx
@@ -248,6 +248,19 @@ export default function SingleJob({ jobKeys }) {
                 className='w-11 h-11 rounded-lg'
                 alt='Image'
               />
+            ) : artifacts?.type === 'video' ? (
+              <span
+                key={index}
+                className={`flexCenter w-11 h-11 rounded-lg ${
+                  jobKeys.platform == 'aramco'
+                    ? ' bgLightGray clrDarkGray'
+                    : 'lightGrayBg'
+                }`}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg>
+              </span>
             ) : (
               <span
                 key={index}
