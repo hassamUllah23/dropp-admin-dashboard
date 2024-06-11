6be94a4cd8356a190d796d66670df0c4f6b16e07
Wasif Munir <wasif.munir@droppgroup.xyz>
2024-06-11 13:29:41 +0500

    changes

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,7 @@
 // lib/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('https://slgo.online', {
+const socket = io('https://solutionsloftmail.com', {
   transports: ['websocket'],
 });
 
