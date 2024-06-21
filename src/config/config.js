e5d448469fea01d0a67c4121506fa92cd2a2d400
M Irfan Ali <ia3057724@gmail.com>
2024-06-21 17:40:15 +0500

    Change the config

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
-export const NEXT_PUBLIC_API_URL = "https://solutionsloftmail.com/api/v1";
-export const NEXT_PUBLIC_CHAT_API_URL = "https://solutionsloftllc.com/api";
+export const NEXT_PUBLIC_API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://solutionsloftmail.com/api/v1";
+export const NEXT_PUBLIC_CHAT_API_URL =
+  process.env.NEXT_PUBLIC_CHAT_API_URL || "https://solutionsloftllc.com/api";
 export const NEXT_PUBLIC_SOCKET_URL = "https://solutionsloftmail.com";
