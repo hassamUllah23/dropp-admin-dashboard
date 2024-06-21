0cabd9bca95cd2cfe24d484966d809fc86d3edb7
M Irfan Ali <ia3057724@gmail.com>
2024-06-21 17:39:29 +0500

    Improve the ui for stats

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
-export const NEXT_PUBLIC_API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://solutionsloftmail.com/api/v1";
-export const NEXT_PUBLIC_CHAT_API_URL =
-  process.env.NEXT_PUBLIC_CHAT_API_URL || "https://solutionsloftllc.com/api";
+export const NEXT_PUBLIC_API_URL = "https://solutionsloftmail.com/api/v1";
+export const NEXT_PUBLIC_CHAT_API_URL = "https://solutionsloftllc.com/api";
 export const NEXT_PUBLIC_SOCKET_URL = "https://solutionsloftmail.com";
