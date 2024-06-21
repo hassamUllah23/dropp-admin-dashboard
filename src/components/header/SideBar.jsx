ea7eaba76cec5723fd78da7dffbabcdd2f02afa4
Hassam Ullah <hassam.ullah@solutionsloft.com>
2024-06-21 17:21:42 +0500

    add stats page

--- src/components/header/SideBar.jsx
+++ src/components/header/SideBar.jsx
@@ -50,6 +50,11 @@ export default function SideBar({ onClose }) {
     onClose();
     router.push('/users');
   };
+  
+  const linkToStatistics = () => {
+    onClose();
+    router.push('/stats');
+  };
 
   const linkToSettings = () => {
     onClose();
@@ -191,6 +196,29 @@ export default function SideBar({ onClose }) {
                 </svg>
 
                 <span className=' text-sm pl-2'> Users list</span>
+              </p>
+            </div>
+            <div className='w-full py-2'>
+              <p
+                className='flex items-middle text-white/80 cursor-pointer'
+                onClick={linkToStatistics}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-4 h-4'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+                  />
+                </svg>
+
+                <span className=' text-sm pl-2'>Statistics</span>
               </p>
             </div>
             {/* <div className="flex items-middle py-2">
