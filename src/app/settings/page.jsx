9d2ec3b5789892036df166bef3fe38af1ca5214c
Hassam Ullah <hassam.ullah@solutionsloft.com>
2024-08-01 19:54:23 +0500

    add photomaker points

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,18 +1,15 @@
-'use client';
-import RewardsForm from '@/components/dashboard/rewards/RewardsForm';
+"use client";
+import RewardsForm from "@/components/dashboard/rewards/RewardsForm";
 
 export default function page() {
-  const deleteMyAccount = () => {};
   return (
-    <div className='divide-y divide-white/5 w-full mx-auto p-2.5  max-w-7xl'>
-      <div className='grid w-full grid-cols-1 gap-x-8 gap-y-10 px-0 md:px-4 py-4 md:py-16 sm:px-6 md:grid-cols-3 lg:px-8'>
-        <div>
-          <h2 className='text-base font-semibold leading-7 text-white'>
-            Rewards Information
-          </h2>
-          <p className='mt-1 text-sm leading-6 text-gray-400'></p>
-        </div>
-
+    <div className="flex flex-col ga-y-5">
+      <div className="flex flex-row justify-center items-center">
+        <h1 className="text-2xl font-semibold leading-7 text-white">
+          Rewards Information
+        </h1>
+      </div>
+      <div className="p-20">
         <RewardsForm />
       </div>
     </div>
--- src/components/dashboard/rewards/RewardsForm.jsx
+++ src/components/dashboard/rewards/RewardsForm.jsx
@@ -1,48 +1,43 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import useApiHook from '@/hooks/useApiHook';
-import { toast } from 'react-toastify';
-import { RotatingLines } from 'react-loader-spinner';
-import * as Yup from 'yup';
+import { useEffect, useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import useApiHook from "@/hooks/useApiHook";
+import { toast } from "react-toastify";
+import { RotatingLines } from "react-loader-spinner";
+import * as Yup from "yup";
 
 const RewardsForm = () => {
-  const [showLoading, setShowLoading] = useState(false);
   const [initialValues, setInitialValues] = useState(null);
   const { handleApiCall, isApiLoading } = useApiHook();
 
   const saveRewards = async (values) => {
-    setShowLoading(true);
     await handleApiCall({
-      method: 'put',
-      url: '/settings/change',
+      method: "put",
+      url: "/settings/change",
       data: values,
     })
       .then((res) => {
         if (res.status === 200) {
-          toast.success('Rewards updated.');
-          setShowLoading(false);
+          toast.success("Settings updated.");
         }
         return res;
       })
       .catch((error) => {
         toast.error(error?.response?.data?.errors);
-        setShowLoading(false);
       });
   };
 
   const getRewards = async () => {
     await handleApiCall({
-      method: 'get',
-      url: '/settings',
+      method: "get",
+      url: "/settings",
     })
       .then((res) => {
         if (res.status === 200) {
           setInitialValues({
             imageCreationPoints: res.data?.imageCreationPoints,
             digitalHumanCreationPoints: res.data?.digitalHumanCreationPoints,
-
             initialParanormaDeduction: res.data?.initialParanormaDeduction,
             refineParanormaDeduction: res.data?.refineParanormaDeduction,
             initialVirtualPoints: res?.data?.initialVirtualPoints,
@@ -60,7 +55,9 @@ const RewardsForm = () => {
             mascotPoints: res.data?.mascotPoints,
             initialParanormaDeduction: res.data?.initialParanormaDeduction,
             refineParanormaDeduction: res.data?.refineParanormaDeduction,
-            settingsId: res?.data?._id,
+            photoSharePoints: res?.data?.photoSharePoints,
+            photoCreatePoints: res?.data?.photoCreatePoints,
+            photoCreationLimit: res?.data?.photoCreationLimit,
           });
         }
         return res;
@@ -75,53 +72,58 @@ const RewardsForm = () => {
   }, []);
 
   const validationSchema = Yup.object().shape({
-    initialVirtualPoints: Yup.number().required('Required'),
-    skyboxGeneration: Yup.number().required('Required'),
-    initialWardDropPoints: Yup.number().required('Required'),
-    shareDiscordPoints: Yup.number().required('Required'),
-    shareTwitterPoints: Yup.number().required('Required'),
-    initialAccountCreationPoints: Yup.number().required('Required'),
-    walletPoints: Yup.number().required('Required'),
-    followDiscordPoints: Yup.number().required('Required'),
-    followTwitterPoints: Yup.number().required('Required'),
-    initialParanormaDeduction: Yup.number().required('Required'),
-    refineParanormaDeduction: Yup.number().required('Required'),
-    digitalHumanCreationPoints: Yup.number().required('Required'),
-    imageCreationPoints: Yup.number().required('Required'),
-    retweetPoints: Yup.number().required('Required'),
-    mascotPoints: Yup.number().required('Required'),
-    tweetUrl: Yup.string().required('Required'),
+    initialVirtualPoints: Yup.number().required("Required"),
+    skyboxGeneration: Yup.number().required("Required"),
+    initialWardDropPoints: Yup.number().required("Required"),
+    shareDiscordPoints: Yup.number().required("Required"),
+    shareTwitterPoints: Yup.number().required("Required"),
+    initialAccountCreationPoints: Yup.number().required("Required"),
+    walletPoints: Yup.number().required("Required"),
+    followDiscordPoints: Yup.number().required("Required"),
+    followTwitterPoints: Yup.number().required("Required"),
+    initialParanormaDeduction: Yup.number().required("Required"),
+    refineParanormaDeduction: Yup.number().required("Required"),
+    digitalHumanCreationPoints: Yup.number().required("Required"),
+    imageCreationPoints: Yup.number().required("Required"),
+    retweetPoints: Yup.number().required("Required"),
+    mascotPoints: Yup.number().required("Required"),
+    tweetUrl: Yup.string().required("Required"),
+    photoSharePoints: Yup.number().required("Required"),
+    photoCreatePoints: Yup.string().required("Required"),
+    photoCreationLimit: Yup.string().required("Required"),
   });
 
   const titles = {
-    initialVirtualPoints: '360 Image Points',
-    skyboxGeneration: 'Max limit of 360 generation',
-    initialWardDropPoints: 'Wardrobe Points',
-    shareDiscordPoints: 'Share on Discord Points',
-    shareTwitterPoints: 'Share on Twitter Points',
-    followDiscordPoints: 'Follow Discord Points',
-    followTwitterPoints: 'Follow Twitter Points',
-    initialAccountCreationPoints: 'Account creation points',
-    walletPoints: 'Wallet Connection Points',
-    tweetUrl: 'Twitter Tweet ID',
-    retweetPoints: 'Twitter Retweet Points',
-    mascotPoints: 'APE SKEE Mascot Points',
-    initialParanormaDeduction: 'Points for Initial 3D Model',
-    refineParanormaDeduction: 'Points for Refining 3D Model',
-    digitalHumanCreationPoints: 'Points for Digital Human',
-    imageCreationPoints: 'Points for Text To Image',
-    settingsId: 'Settings ID',
+    initialVirtualPoints: "360 Image Points",
+    skyboxGeneration: "Max limit of 360 generation",
+    initialWardDropPoints: "Wardrobe Points",
+    shareDiscordPoints: "Share on Discord Points",
+    shareTwitterPoints: "Share on Twitter Points",
+    followDiscordPoints: "Follow Discord Points",
+    followTwitterPoints: "Follow Twitter Points",
+    initialAccountCreationPoints: "Account creation points",
+    walletPoints: "Wallet Connection Points",
+    tweetUrl: "Twitter Tweet ID",
+    retweetPoints: "Twitter Retweet Points",
+    mascotPoints: "APE SKEE Mascot Points",
+    initialParanormaDeduction: "Points for Initial 3D Model",
+    refineParanormaDeduction: "Points for Refining 3D Model",
+    digitalHumanCreationPoints: "Points for Digital Human",
+    imageCreationPoints: "Points for Text To Image",
+    photoSharePoints: "AI Photomaker Sharing Points",
+    photoCreatePoints: "AI Photomaker Creation Points",
+    photoCreationLimit: "AI Photomaker Creation Limit",
   };
 
   const getFieldType = (key) => {
-    if (key === 'tweetUrl') {
-      return 'text';
+    if (key === "tweetUrl") {
+      return "text";
     }
-    return 'number';
+    return "number";
   };
 
   return (
-    <div className='md:col-span-2 w-full block'>
+    <div className="md:col-span-2 w-full block">
       {initialValues ? (
         <Formik
           initialValues={initialValues}
@@ -129,26 +131,20 @@ const RewardsForm = () => {
           validationSchema={validationSchema}
         >
           {({ setFieldValue, values }) => (
-            <Form className='md:col-span-2 w-full block'>
-              <div className='grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6 rewardsCol'>
+            <Form className="md:col-span-2 w-full block">
+              <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6 rewardsCol">
                 {Object.keys(initialValues).map((key) => (
-                  <div className='sm:col-span-3' key={key}>
-                    {key === 'settingsId' ? (
-                      <div></div>
-                    ) : (
-                      <label
-                        htmlFor={key}
-                        className='block text-sm font-medium leading-6 text-white'
-                      >
-                        {titles[key]}
-                      </label>
-                    )}
+                  <div className="sm:col-span-3" key={key}>
+                    <label
+                      htmlFor={key}
+                      className="block text-sm font-medium leading-6 text-white"
+                    >
+                      {titles[key]}
+                    </label>
 
-                    {key === 'settingsId' ? (
-                      <div></div>
-                    ) : key === 'tweetUrl' ? (
-                      <div className='relative'>
-                        <span className='text-white/70 absolute left-2 top-[.55rem] text-base'>
+                    {key === "tweetUrl" ? (
+                      <div className="relative">
+                        <span className="text-white/70 absolute left-2 top-[.55rem] text-base">
                           x.com/droppgroup/status/
                         </span>
                         <Field
@@ -156,7 +152,7 @@ const RewardsForm = () => {
                           name={key}
                           id={key}
                           placeholder={`Enter Tweet Id`}
-                          className='mt-2  text-[.9rem] px-3 block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white leading-6 pl-[11.7rem]'
+                          className="mt-2  text-[.9rem] px-3 block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white leading-6 pl-[11.7rem]"
                         />
                       </div>
                     ) : (
@@ -165,34 +161,34 @@ const RewardsForm = () => {
                         name={key}
                         id={key}
                         placeholder={`Enter ${key}`}
-                        className='mt-2 px-3 block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6'
+                        className="mt-2 px-3 block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
                       />
                     )}
                     <ErrorMessage
                       name={key}
-                      component='div'
-                      className='text-red-600 pt-4'
+                      component="div"
+                      className="text-red-600 pt-4"
                     />
                   </div>
                 ))}
               </div>
 
-              <div className='mt-8 flex'>
+              <div className="mt-8 flex">
                 <button
-                  type='submit'
-                  className='rounded-md bg-Gradient px-10 py-3 text-sm font-semibold text-black shadow-sm'
+                  type="submit"
+                  className="rounded-md bg-Gradient px-10 py-3 text-sm font-semibold text-black shadow-sm"
                 >
-                  {showLoading ? (
+                  {isApiLoading ? (
                     <RotatingLines
                       visible={true}
-                      height='14'
-                      width='14'
-                      color='blue'
-                      strokeWidth='5'
-                      animationDuration='0.75'
-                      ariaLabel='rotating-lines-loading'
+                      height="14"
+                      width="14"
+                      color="blue"
+                      strokeWidth="5"
+                      animationDuration="0.75"
+                      ariaLabel="rotating-lines-loading"
                       wrapperStyle={{}}
-                      wrapperClass=''
+                      wrapperClass=""
                     />
                   ) : (
                     <p>Update Settings</p>
@@ -203,25 +199,25 @@ const RewardsForm = () => {
           )}
         </Formik>
       ) : (
-        <div className='md:col-span-2 w-full block'>
-          <div role='status' className='flexCenter'>
+        <div className="md:col-span-2 w-full block">
+          <div role="status" className="flexCenter">
             <svg
-              aria-hidden='true'
-              className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white'
-              viewBox='0 0 100 101'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-                fill='currentColor'
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
               />
               <path
-                d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-                fill='currentFill'
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
               />
             </svg>
-            <span className='sr-only'>Loading...</span>
+            <span className="sr-only">Loading...</span>
           </div>
         </div>
       )}
