c57b240dbb8d39d8164e20f8323654af90c28ac0
Hassam Ullah <hassam.ullah@solutionsloft.com>
2024-08-16 17:32:34 +0500

    refactor(settings): remove unnecessary reward settings

--- src/components/dashboard/rewards/RewardsForm.jsx
+++ src/components/dashboard/rewards/RewardsForm.jsx
@@ -6,6 +6,7 @@ import useApiHook from "@/hooks/useApiHook";
 import { toast } from "react-toastify";
 import { RotatingLines } from "react-loader-spinner";
 import * as Yup from "yup";
+import { Loader } from "@/components/common";
 
 const RewardsForm = () => {
   const [initialValues, setInitialValues] = useState(null);
@@ -42,7 +43,7 @@ const RewardsForm = () => {
             refineParanormaDeduction: res.data?.refineParanormaDeduction,
             initialVirtualPoints: res?.data?.initialVirtualPoints,
             skyboxGeneration: res?.data?.skyboxGeneration,
-            initialWardDropPoints: res?.data?.initialWardDropPoints,
+            // initialWardDropPoints: res?.data?.initialWardDropPoints,
             shareDiscordPoints: res?.data?.shareDiscordPoints,
             shareTwitterPoints: res?.data?.shareTwitterPoints,
             initialAccountCreationPoints:
@@ -52,10 +53,10 @@ const RewardsForm = () => {
             walletPoints: res.data?.walletPoints,
             retweetPoints: res.data?.retweetPoints,
             tweetUrl: res.data?.tweetUrl,
-            mascotPoints: res.data?.mascotPoints,
+            // mascotPoints: res.data?.mascotPoints,
             initialParanormaDeduction: res.data?.initialParanormaDeduction,
             refineParanormaDeduction: res.data?.refineParanormaDeduction,
-            photoSharePoints: res?.data?.photoSharePoints,
+            // photoSharePoints: res?.data?.photoSharePoints,
             photoCreatePoints: res?.data?.photoCreatePoints,
             photoCreationLimit: res?.data?.photoCreationLimit,
           });
@@ -74,7 +75,7 @@ const RewardsForm = () => {
   const validationSchema = Yup.object().shape({
     initialVirtualPoints: Yup.number().required("Required"),
     skyboxGeneration: Yup.number().required("Required"),
-    initialWardDropPoints: Yup.number().required("Required"),
+    // initialWardDropPoints: Yup.number().required("Required"),
     shareDiscordPoints: Yup.number().required("Required"),
     shareTwitterPoints: Yup.number().required("Required"),
     initialAccountCreationPoints: Yup.number().required("Required"),
@@ -86,9 +87,9 @@ const RewardsForm = () => {
     digitalHumanCreationPoints: Yup.number().required("Required"),
     imageCreationPoints: Yup.number().required("Required"),
     retweetPoints: Yup.number().required("Required"),
-    mascotPoints: Yup.number().required("Required"),
+    // mascotPoints: Yup.number().required("Required"),
     tweetUrl: Yup.string().required("Required"),
-    photoSharePoints: Yup.number().required("Required"),
+    // photoSharePoints: Yup.number().required("Required"),
     photoCreatePoints: Yup.string().required("Required"),
     photoCreationLimit: Yup.string().required("Required"),
   });
@@ -96,7 +97,7 @@ const RewardsForm = () => {
   const titles = {
     initialVirtualPoints: "360 Image Points",
     skyboxGeneration: "Max limit of 360 generation",
-    initialWardDropPoints: "Wardrobe Points",
+    // initialWardDropPoints: "Wardrobe Points",
     shareDiscordPoints: "Share on Discord Points",
     shareTwitterPoints: "Share on Twitter Points",
     followDiscordPoints: "Follow Discord Points",
@@ -105,12 +106,12 @@ const RewardsForm = () => {
     walletPoints: "Wallet Connection Points",
     tweetUrl: "Twitter Tweet ID",
     retweetPoints: "Twitter Retweet Points",
-    mascotPoints: "APE SKEE Mascot Points",
+    // mascotPoints: "APE SKEE Mascot Points",
     initialParanormaDeduction: "Points for Initial 3D Model",
     refineParanormaDeduction: "Points for Refining 3D Model",
     digitalHumanCreationPoints: "Points for Digital Human",
     imageCreationPoints: "Points for Text To Image",
-    photoSharePoints: "AI Photomaker Sharing Points",
+    // photoSharePoints: "AI Photomaker Sharing Points",
     photoCreatePoints: "AI Photomaker Creation Points",
     photoCreationLimit: "AI Photomaker Creation Limit",
   };
@@ -199,27 +200,28 @@ const RewardsForm = () => {
           )}
         </Formik>
       ) : (
-        <div className="md:col-span-2 w-full block">
-          <div role="status" className="flexCenter">
-            <svg
-              aria-hidden="true"
-              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
+        <Loader size={28} />
+        // <div className="md:col-span-2 w-full block">
+        //   <div role="status" className="flexCenter">
+        //     <svg
+        //       aria-hidden="true"
+        //       className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+        //       viewBox="0 0 100 101"
+        //       fill="none"
+        //       xmlns="http://www.w3.org/2000/svg"
+        //     >
+        //       <path
+        //         d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        //         fill="currentColor"
+        //       />
+        //       <path
+        //         d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        //         fill="currentFill"
+        //       />
+        //     </svg>
+        //     <span className="sr-only">Loading...</span>
+        //   </div>
+        // </div>
       )}
     </div>
   );
