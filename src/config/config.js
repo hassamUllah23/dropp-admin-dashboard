fe344f8d6e80065157daa9bf9a8f8ba74d526eb0
M Irfan Ali <ia3057724@gmail.com>
2024-09-11 14:07:56 +0500

    Changes into the config files

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export const NEXT_PUBLIC_API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'https://solutionsloftmail.com/api/v1';
+  process.env.NEXT_PUBLIC_API_URL || 'https://server.dropp-phygital.com/api/v1';
 export const NEXT_PUBLIC_CHAT_API_URL =
-  process.env.NEXT_PUBLIC_CHAT_API_URL || 'https://solutionsloftllc.com/api';
-export const NEXT_PUBLIC_SOCKET_URL = 'https://solutionsloftmail.com';
+  process.env.NEXT_PUBLIC_CHAT_API_URL ||
+  'https://devnet.droppthink.com/api';
+export const NEXT_PUBLIC_SOCKET_URL = 'https://server.dropp-phygital.com';
